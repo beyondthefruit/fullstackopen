009b958a6a11568f132b6cad31905bf6f3aa4357
Kevin Hanard <kevin.hanard@gmail.com>
2023-02-23 10:29:23 +1100

    feat: 3.10

--- part2/phonebook/src/services/phones.js
+++ part2/phonebook/src/services/phones.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3003/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -21,21 +21,5 @@ const deletePhone = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response.data);
 };
-
-// axios.interceptors.response.use(null, (error) => {
-//   if (error.response && error.response.status === 401) {
-//     window.location = '<login-url>';
-//   }
-//   throw error;
-// });
-// axios.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     if (err.response.status === 404) {
-//       throw new Error(`${err.config.url} not found`);
-//     }
-//     throw err;
-//   }
-// );
 
 export default { getAll, create, update, deletePhone };
