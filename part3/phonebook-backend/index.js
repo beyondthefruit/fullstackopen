f18102aad4ed23b22e2cfbc929028207110e27db
Kevin Hanard <kevin.hanard@gmail.com>
2023-02-28 13:56:22 +1100

    feat: 3.13 phonebook database 1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const Person = require('./models/note');
 const app = express();
 var morgan = require('morgan');
 const cors = require('cors');
@@ -16,28 +18,7 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+let persons = [];
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
