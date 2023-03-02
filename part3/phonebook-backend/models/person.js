13602be74ffa99169af184a8595648ba3fa82813
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-02 12:16:23 +1100

    feat: 3.22 lint configuration, all warnings fixed

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -23,12 +23,11 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    required: true,
     validate: {
       validator: function (v) {
         return /\d{4}-\d{3}-\d{3}/.test(v);
       },
-      message: (props) => `${props.value} is not a valid phone number!`,
+      message: 'not a valid phone number!',
     },
     required: [true, 'User phone number required'],
   },
