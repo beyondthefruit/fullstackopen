4911aeea7d2315c0ac598d25a7c01fa7df45ad3b
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-13 14:03:19 +1100

    feat: 4.4 unit test 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,18 @@
+const blog = require('../models/blog');
+
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((acc, currBlog) => {
+    acc += currBlog.likes;
+    return acc;
+  }, 0);
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
