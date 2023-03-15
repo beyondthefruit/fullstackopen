c7614c4c442e11a0b7b65b724f1120167a88d131
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-15 11:55:01 +1100

    test back

--- part3/helloworld/tests/note_api.test.js
+++ part3/helloworld/tests/note_api.test.js
@@ -4,13 +4,14 @@ const app = require('../app');
 
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
+test('notes are returned as json file', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/);
 });
 
+//close database connection used by Mongoose
 afterAll(async () => {
   await mongoose.connection.close();
 });
