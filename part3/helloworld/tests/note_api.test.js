432e8d7740c21aa47794b122823871ae2e49431a
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-16 15:53:01 +1100

    feat: beforeEach

--- part3/helloworld/tests/note_api.test.js
+++ part3/helloworld/tests/note_api.test.js
@@ -11,14 +11,14 @@ const helper = require('./test_helpers');
 beforeEach(async () => {
   await Note.deleteMany({});
 
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note);
+    await noteObject.save();
+  }
 }, 200000);
 
 test('notes are returned as json', async () => {
+  console.log('entered test');
   await api
     .get('/api/notes')
     .expect(200)
