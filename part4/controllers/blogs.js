43d32e57fa7fc247a517002334c90f77da16b39c
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-17 15:23:22 +1100

    feat: 4.10, blog list test step3, test creation of blog posts

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -7,11 +7,24 @@ blogsRouter.get('/', async (request, response, next) => {
 });
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body);
+  // const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+  // blog.save().then((result) => {
+  //   response.status(201).json(result);
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   });
+
+  try {
+    const savedBlogs = await blog.save();
+    response.status(201).json(savedBlogs);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -5,30 +5,13 @@ const app = require('../app');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-];
+const helper = require('./test_helpers');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 }, 20000);
 
@@ -52,6 +35,26 @@ test('that unique identifier is named id', async () => {
   // we have to use [0] to check this property
   expect(response.body[0].id).toBeDefined();
   expect(response.body[0]._id).not.toBeDefined();
+});
+
+test('a valid post can be added', async () => {
+  const newPost = {
+    _id: '5a422aa71b54a676222020200211111',
+    title: 'Testing the revolution',
+    author: 'K.H Poilu',
+    url: 'http://www.u.unilibre.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 15,
+    __v: 0,
+  };
+
+  await api
+    .post('/api/blogs')
+
+    .send(newPost)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 });
 
 //close database connection used by Mongoose
