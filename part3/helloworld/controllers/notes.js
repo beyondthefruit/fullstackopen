d2e900b76fb5ea0419e460d169c350cdd8408183
Kevin Hanard <kevin.hanard@gmail.com>
2023-03-21 16:42:25 +1100

    token aut

--- part3/helloworld/controllers/notes.js
+++ part3/helloworld/controllers/notes.js
@@ -1,25 +1,30 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
   response.json(notes);
 });
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
-
+  const user = await User.findById(body.userId);
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   });
 
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch (exception) {
-    next(exception);
-  }
+  // try {
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+  response.json(savedNote);
+  //   response.status(201).json(savedNote);
+  // } catch (exception) {
+  //   next(exception);
+  // }
 });
 
 notesRouter.get('/:id', async (request, response, next) => {
