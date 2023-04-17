0d50a3799df37ea5bd8feaa24626240ba01bbc65
Kevin Hanard <kevin.hanard@gmail.com>
2023-04-17 15:29:04 +1000

    feat: 3.16

--- part4/app.js
+++ part4/app.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
-
 const usersRouter = require('./controllers/users');
 
 mongoose.set('strictQuery', false);
@@ -30,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -5,6 +5,13 @@ const User = require('../models/user');
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (password.length <= 3) {
+    response.status(400).json({ error: 'min password size is 3 characters' });
+  }
+  if (username.length <= 3) {
+    response.status(400).json({ error: 'min username size is 3 characters' });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
