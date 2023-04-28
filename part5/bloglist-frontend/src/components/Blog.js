52a127a489e2d4ca4f734177ed35c4c1fb2d00e7
Kevin Hanard <kevin.hanard@gmail.com>
2023-04-28 14:17:56 +1000

    feat: 5.7- Blog list frontend, step7- add btn to display details of articles

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [blogDetail, setBlogDetail] = useState(null);
+  const { title, author, likes, url } = blog;
 
-export default Blog
+  const blogStyle = {
+    paddingTop: '1rem',
+    paddingLeft: '1rem',
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: '0.5rem',
+  };
+
+  const blogBtnStyle = {
+    backgroundColor: 'grey',
+    color: 'white',
+    margin: '0.5rem',
+  };
+  const blogExtraStyle = {
+    paddingLeft: '1rem',
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: '0.5rem',
+  };
+  return (
+    <div>
+      <div style={blogStyle}>
+        {title}
+        <button style={blogBtnStyle} onClick={() => setBlogDetail(!blogDetail)}>
+          {!blogDetail ? 'view' : 'hide'}
+        </button>
+      </div>
+      {blogDetail && (
+        <div style={blogExtraStyle}>
+          <p>{author}</p>
+          <p>link: {url}</p>
+          <p>
+            likes: {likes} <button>Like</button>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
